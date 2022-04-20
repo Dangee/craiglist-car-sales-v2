7f712fe259d5d1473eaa94918ddac1e2f69b710f
dgouin <danielgouin@gmail.com>
2022-04-19 22:05:15 -0700

    APP v1

--- model.py
+++ model.py
@@ -4,40 +4,41 @@ Setup prediction model params and initialization
 """
 
 import params as p
+import numpy as np
 
 # create a dictionary of model intercept and coefficient values
-def initialize_predictor_model(intercept, predictor_vars, predictor_coeff):
+def initialize_prediction_model(intercept, predictor_vars, predictor_coeff):
     predictor_model = {'intercept': intercept}
-    for v in range(0, len(predictor_vars)):
+    for v in range(0,len(predictor_vars)):
         predictor_model[predictor_vars[v]] = predictor_coeff[v]
     return predictor_model
 
-
 # reset user input
 def reset_model_input(predictor_vars):
-    model_input = {}
-    for v in range(0, len(predictor_vars)):
+    model_input = {'intercept': 1}
+    for v in range(0,len(predictor_vars)):
         model_input[predictor_vars[v]] = 0
-    model_input['intercept'] = 1
     return model_input
 
-
 # generate prediction based on user input
-def generate_prediction(predictor_model, model_input):
-    prediction = 0
-    for i in predictor_model.keys():
-        prediction += predictor_model[i] * model_input[i]
+def generate_prediction(predictor_model, predictor_vars, model_input):
+    # convert model_input
+    input_array = []
+    for v in predictor_vars:
+        if v != 'intercept':
+            input_array.append(model_input[v])
+
+    input_array = np.array(input_array).reshape(1, -1)
+    std_input_array = p.std_scaler.transform(input_array)
+
+    prediction_value = p.lin_reg.predict(std_input_array)
+    prediction = round(prediction_value[0],2)
+
     return prediction
 
 
 # build the regression equation description
-PREDICTOR_MODEL = initialize_predictor_model(p.INTERCEPT, p.PREDICTOR_VARS, p.PREDICTOR_COEFF)
+PREDICTOR_MODEL = initialize_prediction_model(p.INTERCEPT,
+                                               p.PREDICTOR_VARS,
+                                               p.PREDICTOR_COEFF)
 
-REGRESSION_EQUATION = "Recommended Sale Price = "
-for v in PREDICTOR_MODEL.keys():
-    if v == "intercept":
-        REGRESSION_EQUATION = REGRESSION_EQUATION + str(round(PREDICTOR_MODEL[v],5)) + " + "
-    else:
-        REGRESSION_EQUATION = REGRESSION_EQUATION + "(" + str(round(PREDICTOR_MODEL[v],5)) + " X " + v + ") + "
-
-REGRESSION_EQUATION = REGRESSION_EQUATION[:-3]
--- params.py
+++ params.py
@@ -3,65 +3,59 @@ Params library
 Define custom parameters for main app
 """
 
+import pickle
+import sklearn
+
 # Application log level
 LOG_LEVEL = 'DEBUG'
-LOG_LEVEL = 'INFO'
+# LOG_LEVEL = 'INFO'
+
+# read in predictor model objects
+with open('./analysis/components_data/predictor_model.pkl', 'rb') as f:
+  predictor_model = pickle.load(f)
+
+with open('./analysis/components_data/std_scaler.pkl', 'rb') as f:
+  std_scaler = pickle.load(f)
+
+with open('./analysis/components_data/lin_reg.pkl', 'rb') as f:
+  lin_reg = pickle.load(f)
+
+with open('./analysis/components_data/predictor_model.pkl', 'rb') as f:
+  predictor_model = pickle.load(f)
+
+with open('./analysis/components_data/app_categories.pkl', 'rb') as f:
+  app_categories = pickle.load(f)
+
+
+# Prediction model parameters
+INTERCEPT = predictor_model['lin_reg']['intercept']
+PREDICTOR_VARS = predictor_model['predictor_vars']
+PREDICTOR_COEFF = predictor_model['lin_reg']['coeff']
+
 
 # Application pull down selections
-FUEL_TYPE = ['gas',  'diesel', 'hybrid', 'electric', 'other']
-CAR_TYPE = ['sedan', 'coupe', 'convertible', 'hatchback', 'wagon', 'mini-van', 'van', 'SUV', 'pickup', 'truck',
-       'offroad', 'bus', 'other_type']
-CAR_CONDITION = ['new', 'like new', 'excellent', 'good', 'fair',  'salvage']
-TRANSMISSION = ['automatic', 'manual', 'other']
-
+FUEL_TYPE = app_categories['fuel_type']
+TRANSMISSION = app_categories['transmission']
+DRIVE = app_categories['drive']
+CAR_TYPE = app_categories['car_type']
+CAR_CONDITION = app_categories['car_condition']
+ODOMETER_CAT = app_categories['odometer_cat']
 
 # Car website URL:
 CAR_WEBSITE = 'https://sfbay.craigslist.org/search/cta'
-# search URL https://sfbay.craigslist.org/search/cta?query=mustang+gt+convertible
 IMAGE_URL = 'https://www.conceptcarz.com/'
 
 # Google sheet with test data
-GOOGLE_SHEET_TEST_DATA = 'https://docs.google.com/spreadsheets/d/11VuF8B9UZ8jdRc-3hjzTgghEVlblzMDD3TeUNmioNok/edit?usp=sharing'
+GOOGLE_SHEET_TEST_DATA = None
 
-# Prediction model parameters
-INTERCEPT = -769538.86433 # cheat - add adjustment factor to avoid negative pricing
-# INTERCEPT = -804538.8643260737
-
-PREDICTOR_VARS = ['year',
- 'odometer',
- 'ferrari',
- 'gas',
- 'diesel',
- 'other_fuel',
- 'sedan',
- 'pickup',
- 'other_type',
- 'automatic',
- 'other_transmission',
- 'excellent',
- 'good',
- 'fair']
-
-PREDICTOR_COEFF = [390.5031441248841,
- -0.017659688952919708,
- 103920.36002700841,
- 1745.5726762019538,
- 13946.538609987403,
- 5743.117645250859,
- -4301.212153910639,
- 8570.283187851448,
- 3708.44941088399,
- -1170.7571963590008,
- 3942.8207817679704,
- -2559.9572273672566,
- -3043.6496077529555,
- -9014.598056518584]
 
 VERBATIMS = {
   'no_market': [
     "oh dude, I am afraid there is no market for this car. Sorry!",
     "ah, if you pay someone, they may agree to take if off your hands",
-    "your car maybe worth more by selling it in the parts :-)"
+    "your car maybe worth more by selling it in the parts :-)",
+    "sorry to be bearer of bad news, but your car is not worth much :-(",
+    "oh boy, maybe you should donate this car instead of selling it"
  ],
   'ferrari': [
      "Ferrari - the great sports car legacy!",
@@ -70,7 +64,9 @@ VERBATIMS = {
      "hey, wait a minute - this should hold on to this car now? This will likely continue to increase in value!"
   ],
  'cheap': [
-    "well, nobody will break the bank for this one!"
+    "well, nobody will break the bank for this one!",
+    "ah, look at that, what a deal!",
+    "is that what we call a clunker?"
  ],
  'antique': [
    "Ok, well, this is really an antique car",
@@ -79,7 +75,7 @@ VERBATIMS = {
  ],
  'classic': [
    "ah, this is a classic car",
-   "wait, this is really a classic car. It might continue to increase in value if you hold on to it"
+   "wait, this is really a classic car. It might continue to increase in value if you hold on to it",
  ]
 }
 
